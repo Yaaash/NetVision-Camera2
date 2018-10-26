44bfa4847a0ee505bafe24bcb9ff7c94633c761f
Yashika <ykyashikakatyal@gmail.com>
2018-10-26 18:15:42 +0530

    code initialization

---
+++ app/src/main/java/com/netvirta/netvisioncamera2/WelcomeActivity.kt
@@ -0,0 +1,30 @@
+package com.netvirta.netvisioncamera2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
